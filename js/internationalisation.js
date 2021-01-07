2e2024ac8e4738a1359431fb64838ce08fc7532d
Konstantin Tuev <kmt@munich.com>
2021-01-07 14:01:11 +0200

    Support GitHub Pages

--- js/internationalisation.js
+++ js/internationalisation.js
@@ -66,6 +66,13 @@ function handleNewLang() {
             translateStrings();
         }
     };
-    xmlhttp.open("GET", "/translations/strings_"+lang+".json", true);
+    let url = new URL(document.location.href);
+    let target = url.protocol+"//"+url.hostname +":"+ url.port;
+    if (url.pathname.endsWith(".html")) {
+        target += url.pathname.substr(0, url.pathname.lastIndexOf("/"));
+    } else {
+        target += url.pathname;
+    }
+    xmlhttp.open("GET", target+"/translations/strings_"+lang+".json", true);
     xmlhttp.send();
 }
